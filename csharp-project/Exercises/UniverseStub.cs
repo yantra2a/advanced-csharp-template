2cb7227d3d04533d4c8c90163c3417bd843204e5
yannick <yantra2a@sti-2d.com>
2018-09-16 11:38:09 +0200

    Update UniverseStub.cs

--- csharp-project/Exercises/UniverseStub.cs
+++ csharp-project/Exercises/UniverseStub.cs
@@ -7,12 +7,7 @@ namespace Answer
 	public class UniverseStub
 	{
 // }
-public static int CountAllStars(params int[] galaxies){
-	int totalStars = 0;
-	foreach(int stars in galaxies) {
-		totalStars = stars; // fix me!
-	} 
-	return totalStars;
+
 }
 //{ autofold
 	}
